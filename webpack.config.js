7ccd6d97eb16e0376475539cc44371759d2accb5
dnzk <danishkalesaran@gmail.com>
2016-09-29 18:00:42 +0700

    add build script

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
