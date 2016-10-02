59db970a0f806e4bc98965fdef42b17363f77d0d
dnzk <danishkalesaran@gmail.com>
2016-10-02 21:56:46 +0700

    remove bloats in app.js

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -8,24 +8,8 @@ import Backdrop from './backdrop';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { root: [] };
-  }
-  componentDidMount() {
-    axios.get('http://swapi.co/api/')
-      .then(res => {
-        this.setState({ root: res.data });
-      });
   }
   render() {
-    let characters = [
-      'Anakin Skywalker',
-      'Luke Skywalker',
-      'Headhunterz',
-      'Son go ku',
-      'Saitama',
-      'Mister Hitman'
-    ];
-
     return (
       <div id="content">
         <Sidebar />
